86aa222e8c470f09da29d643286db371f758b185
gonullubankasi <muhammetabdullah.soyturk@agu.edu.tr>
2017-11-26 21:47:40 +0300

    changes

--- app.js
+++ app.js
@@ -32,4 +32,6 @@ Router.use("/users/:uid/houses/:hid/members", members);
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 app.use('/api', Router);
-app.listen(3000);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
