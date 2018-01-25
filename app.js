2842b6d0431a8656b049074c6395a7ce4edfc901
gonullubankasi <muhammetabdullah.soyturk@agu.edu.tr>
2018-01-25 12:08:52 +0300

    change

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ var express = require('express'),
     members    = require('./routes/members'),
     signup     = require('./routes/signup');
 
-mongoose.connect("mongodb://localhost/houseassistant");
-//mongoose.connect('mongodb://muhammet:123@ds121716.mlab.com:21716/communicator');
+//mongoose.connect("mongodb://localhost/houseassistant");
+mongoose.connect('mongodb://muhammet:123@ds121716.mlab.com:21716/communicator');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
